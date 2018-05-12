62ef33ebc8fbef6dcf9498c558e3993364c8e329
Guilherme <guilherme.dmatos@hotmail.com>
2018-05-12 16:48:01 -0300

    aux

---
+++ src/main/java/br/edu/ifsc/univerg/model/AuxClass.java
@@ -0,0 +1,25 @@
+package br.edu.ifsc.univerg.model;
+
+public class AuxClass {
+	static String aux;
+	static Boolean val=false;
+
+	public static String getAux() {
+		return aux;
+	}
+
+	public static void setAux(String aux) {
+		AuxClass.aux = aux;
+	}
+
+	public static Boolean getVal() {
+		return val;
+	}
+
+	public static void setVal(Boolean val) {
+		AuxClass.val = val;
+	}
+	
+	
+
+}
