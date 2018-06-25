d04f0fb5cc8e57ceb717ce86a7bb3ec862efbd3f
Guilherme <guilherme.dmatos@hotmail.com>
2018-06-25 01:03:56 -0300

    test

--- src/main/java/br/edu/ifsc/univerg/model/DisciplinaModel.java
+++ src/main/java/br/edu/ifsc/univerg/model/DisciplinaModel.java
@@ -5,6 +5,7 @@ public class DisciplinaModel {
 	private int id_curso;
 	private String nome;
 	private String nomeCurso;
+	private String ementa;
 	public DisciplinaModel(int id, int id_curso, String nome) {
 		this.id = id;
 		this.id_curso = id_curso;
@@ -17,13 +18,15 @@ public class DisciplinaModel {
 		this.nomeCurso = nomeCurso;
 	}
 
-	public DisciplinaModel(String nome, String nomeCurso) {
+	public DisciplinaModel(String nome, String nomeCurso, String ementa) {
 		this.nome = nome;
 		this.nomeCurso = nomeCurso;
+		this.ementa=ementa;
 	}
 
-	public DisciplinaModel(String nome) {
+	public DisciplinaModel(String nome, String ementa) {
 		this.nome = nome;
+		this.ementa=ementa;
 	}
 	public int getId() {
 		return id;
@@ -50,6 +53,15 @@ public class DisciplinaModel {
 	public void setNomeCurso(String nomeCurso) {
 		this.nomeCurso = nomeCurso;
 	}
+	
+	public String getEmenta() {
+		return ementa;
+	}
+
+	public void setEmenta(String ementa) {
+		this.ementa = ementa;
+	}
+
 	@Override
 	public String toString() {
 		return "DisciplinaModel [id=" + id + ", id_curso=" + id_curso + ", nome=" + nome + ", nomeCurso=" + nomeCurso
